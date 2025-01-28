fe8ff84d68a56394e996047ce9bc1d9d0277cc27
Arina Bebko <bebkoarina1@gmail.com>
2025-01-28 12:31:59 +0500

    18 без бд

--- Bebko_41/ProductPage.xaml.cs
+++ Bebko_41/ProductPage.xaml.cs
@@ -23,11 +23,86 @@ namespace Bebko_41
         public ProductPage()
         {
             InitializeComponent();
+            var currentProducts = Bebko_41Entities.GetContext().Product.ToList();
+            ProductListView.ItemsSource = currentProducts;
+
+            ComboType.SelectedIndex = 0;
+            Updt();
         }
+
+
+        private void Updt()
+        {
+            var currentProducts = Bebko_41Entities.GetContext().Product.ToList();
+
+
+
+            if (ComboType.SelectedIndex == 0)
+            {
+                currentProducts = currentProducts.Where(p => (Convert.ToInt32(p.ProductDiscountAmount) >= 0 && Convert.ToInt32(p.ProductDiscountAmount) <= 100)).ToList();
+            }
+            if (ComboType.SelectedIndex == 1)
+            {
+                currentProducts = currentProducts.Where(p => (Convert.ToInt32(p.ProductDiscountAmount) >= 0 && Convert.ToInt32(p.ProductDiscountAmount) < 10)).ToList();
+            }
+            if (ComboType.SelectedIndex == 2)
+            {
+                currentProducts = currentProducts.Where(p => (Convert.ToInt32(p.ProductDiscountAmount) >= 10 && Convert.ToInt32(p.ProductDiscountAmount) < 15)).ToList();
+            }
+            if (ComboType.SelectedIndex == 3)
+            {
+                currentProducts = currentProducts.Where(p => (Convert.ToInt32(p.ProductDiscountAmount) >= 15)).ToList();
+            }
+
+
+            currentProducts=currentProducts.Where(p => (p.ProductName.ToLower().Contains(TBSearch.Text.ToLower()))).ToList();
+
+
+            ProductListView.ItemsSource = currentProducts.ToList();
+
+
+            if (RButtonDown.IsChecked.Value)
+                ProductListView.ItemsSource = currentProducts.OrderByDescending(p => p.ProductCost).ToList();
+            if (RButtonUp.IsChecked.Value)
+                ProductListView.ItemsSource = currentProducts.OrderBy(p => p.ProductCost).ToList();
+            UpdateCountDisplay(currentProducts.Count);
+        }
+        int totalCount;
+
+        private void UpdateCountDisplay(int filteredCount)
+        {
+
+            int totalCount = Bebko_41Entities.GetContext().Product.Count(); // Общее количество записей в базе
+            CountText.Text = $"Количество: {filteredCount} из {totalCount}";
+        }
+
+
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Manager.MainFrame.Navigate(new AddEditPage());
+        }
+
+        private void TBSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Updt();
+
+        }
+
+        private void ComboType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Updt();
+        }
+
+  
+        private void RButtonUp_Checked(object sender, RoutedEventArgs e)
+        {
+            Updt();
+        }
+
+        private void RButtonDown_Checked(object sender, RoutedEventArgs e)
+        {
+            Updt();
         }
     }
 }
